e409342a03f6c21f399e60a4e9fe29cbec8ec061
Choi Seungeon <chou610@naver.com>
2019-05-12 19:34:13 +0900

    Add minimum layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import TreeController from './controller/treeController'
 function component () {
-    var element = document.createElement('div');
+    let element = document.createElement('div');
     element.innerHTML = new TreeController().getData();
     return element;
 }
